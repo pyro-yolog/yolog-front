97a60d1bb4938fd2b4974b3a95174cb3c8bf88a6
정성휘 <whistle_js@naver.com>
2024-06-15 01:53:28 +0900

    refactor: colors constants 분리 작업

--- app/components/diary-book/create-cover-palette.tsx
+++ app/components/diary-book/create-cover-palette.tsx
@@ -1,28 +1,11 @@
+import COLORS from '@/app/lib/constants/colors';
+
 interface Props {
   selectColor?: string;
   onChangeColor: (color: string) => void;
 }
 
 function DiaryBookCreateCoverPalette({ selectColor, onChangeColor }: Props) {
-  const COLORS = [
-    '#FFFFB5',
-    '#D2F5F5',
-    '#C6DCE9',
-    '#9DAAA2',
-    '#C6EFDC',
-    '#FED5CF',
-    '#FFD1DB',
-    '#BBA498',
-    '#F2E3C7',
-    '#C4D4B1',
-    '#B2BDA8',
-    '#A49D92',
-    '#C6DBDA',
-    '#F1B598',
-    '#D3C7E6',
-    '#ECD5E3',
-  ];
-
   return (
     <div className="grid grid-cols-4 gap-x-17pxr gap-y-15pxr w-full p-20pxr bg-[#F4F4F4] rounded-[20px]">
       {COLORS.map((color) => (
