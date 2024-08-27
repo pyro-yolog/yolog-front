225c971373bd416d84f62d616d12c4233239047e
정성휘 <whistle_js@naver.com>
2024-08-28 02:46:31 +0900

    fix: 빌드 오류 수정

--- app/components/middleware.tsx
+++ app/components/middleware.tsx
@@ -1,12 +1,11 @@
 'use client';
 
 import { tokenState } from '@/lib/store/user';
-import { redirect, usePathname, useRouter } from 'next/navigation';
+import { redirect, usePathname } from 'next/navigation';
 import { useRecoilValue } from 'recoil';
 
-const Middleware = ({ children }: { children: JSX.Element }) => {
+const Middleware = ({ children }: { children: React.ReactNode }) => {
   const { accessToken } = useRecoilValue(tokenState);
-  // const router = useRouter();
   const pathname = usePathname();
 
   // 로그인 사용자 확인
